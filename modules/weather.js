a283a6cfa5af66d6b9d0d66967ab6a430dd1e140
bearity <bearity3@gmail.com>
2016-06-26 21:46:23 +0900

    weather wip

--- modules/weather.js
+++ modules/weather.js
@@ -1,6 +1,19 @@
 "use strict";
-const URL_LIST = "http://www.drk7.jp/weather/";
+const URL = "http://www.drk7.jp/weather/json/13.js";
+const request = require('../lib/request.js');
+const $ = require('jquery');
 
-module.exports = function(args, conn, bot, message) {
+// module.exports = function(args, conn, bot, message) {
+    request.json('weather', URL).then(function(res) {
+        res = res.replace(/drk7jpweather\.callback\(/,'');
+        res = res.replace(/\)\;/,'');
 
-}
+        let json = JSON.parse(res);
+
+        console.log(json.pref.area.東京地方);
+        console.log(json.pref.area.東京地方.info[0].rainfallchance);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+// }
